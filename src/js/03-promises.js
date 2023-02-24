f569312e1901ef54bf2e9d92b5756af21760ffdd
marrakeshn <nesveteymark@gmail.com>
2023-02-24 12:23:25 +0200

    done task 3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,46 @@
+const formEl = document.querySelector('.form');
+let delayInput = null;
+let stepInput = null;
+let amountInput = null;
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const shouldResolve = Math.random() > 0.3;
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
 }
+
+const submitHandler = e => {
+  e.preventDefault();
+  if (!e.target.tagName === 'BUTTON') return;
+
+  const {
+    elements: { delay, step, amount },
+  } = e.currentTarget;
+
+  delayInput = Number(delay.value);
+  stepInput = Number(step.value);
+  amountInput = Number(amount.value);
+
+  for (let i = 1; i <= amountInput; i++) {
+    createPromise(i, delayInput)
+      .then(({ position, delay }) => {
+        console.log(`✅ Fulfilled promise ${position} in ${delay}ms`)
+      })
+      .catch(({ position, delay }) => {
+        console.log(`❌ Rejected promise ${position} in ${delay}ms`)
+      });
+
+    delayInput += stepInput;
+  }
+
+  e.currentTarget.reset();
+};
+
+formEl.addEventListener('submit', submitHandler);
